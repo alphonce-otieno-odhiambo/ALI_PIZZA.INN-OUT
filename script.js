b27cf93a8ee6320fbb13690d8cbd756b7d45d37d
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2021-09-20 15:18:20 +0300

    place the slide and the show

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $("#form-table1").click(function() {
+        $("#type-pizza").show();
+    });
+    $("#form-table1").click(function() {
+        $("#type-pizza").show();
+    });
+
+});
+$(document).ready(function() {
+    $("#form-table2").click(function() {
+        $("#sizes").slideDown();
+    });
+});
